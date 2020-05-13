1b09a450ec8fe0a0e58530ff4d02735d11a9b1cc
David Parks <davidthomasparks@gmail.com>
2020-05-13 11:42:13 -0500

    bag pricing section

--- components/PricingSection.vue
+++ components/PricingSection.vue
@@ -1,111 +1,249 @@
 <template>
-<div class="bg-gray-100">
-  <div class="pt-12 sm:pt-16 lg:pt-20">
-    <div class="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
-      <div class="text-center">
-        <h2 class="text-3xl leading-9 font-extrabold text-gray-900 sm:text-4xl sm:leading-10 lg:text-5xl lg:leading-none">
-          Join the Buddy Joe Coffee Club. 
-        </h2>
-        <p class="mt-4 text-xl leading-7 text-gray-500">
-          Get your coffee monthly without being pressed for time
-        </p>
-      </div>
+<div class="bg-gray-900">
+  <div class="pt-12 px-4 sm:px-6 lg:px-8 lg:pt-20">
+    <div class="text-center">
+      <h2 class="text-lg leading-6 font-semibold text-gray-300 uppercase tracking-wider">
+        Pricing
+      </h2>
+      <p class="mt- text-3xl leading-9 font-extrabold text-white sm:text-4xl sm:leading-10 lg:text-5xl lg:leading-none">
+        Join the Buddy Joe Coffee Club
+      </p>
+      <p class="mt-3 max-w-4xl mx-auto text-xl leading-7 text-gray-300 sm:mt-5 sm:text-2xl sm:leading-8">
+        Get your coffee monthly without being pressed for time.
+      </p>
     </div>
   </div>
-  <div class="mt-8 bg-primary-500 pb-16 sm:mt-12 sm:pb-20 lg:pb-28">
-    <div class="relative">
-      <div class="absolute inset-0 h-1/2 bg-gray-100"></div>
-      <div class="relative max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="max-w-lg mx-auto rounded-lg shadow-lg overflow-hidden lg:max-w-none lg:flex">
-          <div class="bg-white px-6 py-8 lg:flex-shrink-1 lg:p-12">
-            <h3 class="text-2xl leading-8 font-extrabold text-gray-900 sm:text-3xl sm:leading-9">
-              Buddy Joe Coffee Club Membership
-            </h3>
-            <p class="mt-6 text-base leading-6 text-gray-500">
-              Lorem ipsum dolor sit amet consect etur adipisicing elit. Itaque amet indis perferendis blanditiis repellendus etur quidem assumenda.
-            </p>
-            <div class="mt-8">
-              <div class="flex items-center">
-                <h4 class="flex-shrink-0 pr-4 bg-white text-sm leading-5 tracking-wider font-semibold uppercase text-primary-600">
-                  What's included
-                </h4>
-                <div class="flex-1 border-t-2 border-gray-200"></div>
+
+  <div class="mt-16 bg-white pb-12 lg:mt-20 lg:pb-20">
+    <div class="relative z-0">
+      <div class="absolute inset-0 h-5/6 bg-gray-900 lg:h-2/3"></div>
+      <div class="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="relative lg:grid lg:grid-cols-7">
+          <div class="mx-auto max-w-md lg:mx-0 lg:max-w-none lg:col-start-1 lg:col-end-3 lg:row-start-2 lg:row-end-3">
+            <div class="h-full flex flex-col rounded-lg shadow-lg overflow-hidden lg:rounded-none lg:rounded-l-lg">
+              <div class="flex-1 flex flex-col">
+                <div class="bg-white px-6 py-10">
+                  <div>
+                    <h2 class="text-center text-2xl leading-8 font-medium text-gray-900">
+                      1 Bag
+                    </h2>
+                    <div class="mt-4 flex items-center justify-center">
+                      <span class="px-3 flex items-start text-6xl leading-none tracking-tight text-gray-900">
+                        <span class="mt-2 mr-2 text-4xl font-medium">
+                          $
+                        </span>
+                        <span class="font-extrabold">
+                          15
+                        </span>
+                      </span>
+                      <span class="text-xl leading-7 font-medium text-gray-400">
+                        /month
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <div class="flex-1 flex flex-col justify-between border-t-2 border-gray-100 p-6 bg-gray-50 sm:p-10 lg:p-6 xl:p-10">
+                  <ul>
+                    <li class="flex items-start">
+                      <div class="flex-shrink-0">
+                        <svg class="h-6 w-6 text-green-500" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                        </svg>
+                      </div>
+                      <p class="ml-3 text-base leading-6 font-medium text-gray-500">
+                        Access to preview blends
+                      </p>
+                    </li>
+                    <li class="mt-4 flex items-start">
+                      <div class="flex-shrink-0">
+                        <svg class="h-6 w-6 text-green-500" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                        </svg>
+                      </div>
+                      <p class="ml-3 text-base leading-6 font-medium text-gray-500">
+                        Upgrade any time
+                      </p>
+                    </li>
+                    <li class="mt-4 flex items-start">
+                      <div class="flex-shrink-0">
+                        <svg class="h-6 w-6 text-green-500" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                        </svg>
+                      </div>
+                      <p class="ml-3 text-base leading-6 font-medium text-gray-500">
+                        24/7 delivery
+                      </p>
+                    </li>
+                  </ul>
+                  <div class="mt-8">
+                    <div class="rounded-lg shadow-md">
+                      <a href="#" class="block w-full text-center rounded-lg border border-transparent bg-white px-6 py-3 text-base leading-6 font-medium text-primary-600 hover:text-primary-500 focus:outline-none focus:shadow-outline transition ease-in-out duration-150">
+                        Start your trial
+                      </a>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <ul class="mt-8 lg:grid lg:grid-cols-2 lg:col-gap-8 lg:row-gap-5">
-                <li class="flex items-start lg:col-span-1">
-                  <div class="flex-shrink-0">
-                    <svg class="h-5 w-5 text-green-400" fill="currentColor" viewBox="0 0 20 20">
-                      <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"/>
-                    </svg>
-                  </div>
-                  <p class="ml-3 text-sm leading-5 text-gray-700">
-                    Coffee delivered to your door every month
-                  </p>
-                </li>
-                <li class="mt-5 flex items-start lg:col-span-1 lg:mt-0">
-                  <div class="flex-shrink-0">
-                    <svg class="h-5 w-5 text-green-400" fill="currentColor" viewBox="0 0 20 20">
-                      <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"/>
-                    </svg>
-                  </div>
-                  <p class="ml-3 text-sm leading-5 text-gray-700">
-                    Cancel any time
-                  </p>
-                </li>
-                <li class="mt-5 flex items-start lg:col-span-1 lg:mt-0">
-                  <div class="flex-shrink-0">
-                    <svg class="h-5 w-5 text-green-400" fill="currentColor" viewBox="0 0 20 20">
-                      <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"/>
-                    </svg>
-                  </div>
-                  <p class="ml-3 text-sm leading-5 text-gray-700">
-                    Preview new blends before anyone else
-                  </p>
-                </li>
-                <li class="mt-5 flex items-start lg:col-span-1 lg:mt-0">
-                  <div class="flex-shrink-0">
-                    <svg class="h-5 w-5 text-green-400" fill="currentColor" viewBox="0 0 20 20">
-                      <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"/>
-                    </svg>
-                  </div>
-                  <p class="ml-3 text-sm leading-5 text-gray-700">
-                    Discounts & special offers
-                  </p>
-                </li>
-              </ul>
             </div>
           </div>
-          <div class="py-8 px-6 text-center bg-gray-50 lg:flex-shrink-0 lg:flex lg:flex-col lg:justify-center lg:p-12">
-            <p class="text-lg leading-6 font-medium text-gray-900">
-              Pay once, own it forever
-            </p>
-            <div class="mt-4 flex items-center justify-center text-5xl leading-none font-extrabold text-gray-900">
-              <span>
-                $349
-              </span>
-              <span class="ml-3 text-xl leading-7 font-medium text-gray-500">
-                USD
-              </span>
-            </div>
-            <p class="mt-4 text-sm leading-5">
-              <a href="#" class="font-medium text-gray-500 underline">
-                Learn about our membership policy
-              </a>
-            </p>
-            <div class="mt-6">
-              <div class="rounded-md shadow">
-                <a href="#" class="flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-gray-900 hover:bg-gray-800 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
-                  Get Access
-                </a>
+          <div class="mt-10 max-w-lg mx-auto lg:mt-0 lg:max-w-none lg:mx-0 lg:col-start-3 lg:col-end-6 lg:row-start-1 lg:row-end-4">
+            <div class="relative z-10 rounded-lg shadow-xl">
+              <div class="pointer-events-none absolute inset-0 rounded-lg border-2 border-primary-600"></div>
+              <div class="absolute inset-x-0 top-0 transform translate-y-px">
+                <div class="flex justify-center transform -translate-y-1/2">
+                  <span class="inline-flex rounded-full bg-primary-600 px-4 py-1 text-sm leading-5 font-semibold tracking-wider uppercase text-white">
+                    Most popular
+                  </span>
+                </div>
+              </div>
+              <div class="bg-white rounded-t-lg px-6 pt-12 pb-10">
+                <div>
+                  <h2 class="text-center text-3xl leading-9 font-semibold text-gray-900 sm:-mx-6">
+                    3 Bags
+                  </h2>
+                  <div class="mt-4 flex items-center justify-center">
+                    <span class="px-3 flex items-start text-6xl leading-none tracking-tight text-gray-900 sm:text-6xl">
+                      <span class="mt-2 mr-2 text-4xl font-medium">
+                        $
+                      </span>
+                      <span class="font-extrabold">
+                        40
+                      </span>
+                    </span>
+                    <span class="text-2xl leading-8 font-medium text-gray-400">
+                      /month
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div class="border-t-2 border-gray-100 rounded-b-lg pt-10 pb-8 px-6 bg-gray-50 sm:px-10 sm:py-10">
+                <ul>
+                  <li class="flex items-start">
+                    <div class="flex-shrink-0">
+                      <svg class="h-6 w-6 text-green-500" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"/>
+                      </svg>
+                    </div>
+                    <p class="ml-3 text-base leading-6 font-medium text-gray-500">
+                      Quia rem est sed impedit magnam
+                    </p>
+                  </li>
+                  <li class="mt-4 flex items-start">
+                    <div class="flex-shrink-0">
+                      <svg class="h-6 w-6 text-green-500" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"/>
+                      </svg>
+                    </div>
+                    <p class="ml-3 text-base leading-6 font-medium text-gray-500">
+                      Dolorem vero ratione voluptates
+                    </p>
+                  </li>
+                  <li class="mt-4 flex items-start">
+                    <div class="flex-shrink-0">
+                      <svg class="h-6 w-6 text-green-500" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"/>
+                      </svg>
+                    </div>
+                    <p class="ml-3 text-base leading-6 font-medium text-gray-500">
+                      Qui sed ab doloribus voluptatem dolore
+                    </p>
+                  </li>
+                  <li class="mt-4 flex items-start">
+                    <div class="flex-shrink-0">
+                      <svg class="h-6 w-6 text-green-500" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"/>
+                      </svg>
+                    </div>
+                    <p class="ml-3 text-base leading-6 font-medium text-gray-500">
+                      Laborum commodi molestiae id et fugiat
+                    </p>
+                  </li>
+                  <li class="mt-4 flex items-start">
+                    <div class="flex-shrink-0">
+                      <svg class="h-6 w-6 text-green-500" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"/>
+                      </svg>
+                    </div>
+                    <p class="ml-3 text-base leading-6 font-medium text-gray-500">
+                      Nam ut ipsa nesciunt culpa modi dolor
+                    </p>
+                  </li>
+                </ul>
+                <div class="mt-10">
+                  <div class="rounded-lg shadow-md">
+                    <a href="#" class="block w-full text-center rounded-lg border border-transparent bg-primary-600 px-6 py-4 text-xl leading-6 font-medium text-white hover:bg-primary-500 focus:outline-none focus:border-primary-700 focus:shadow-outline-primary transition ease-in-out duration-150">
+                      Start your trial
+                    </a>
+                  </div>
+                </div>
               </div>
             </div>
-            <div class="mt-4 text-sm leading-5">
-              <a href="#" class="font-medium text-gray-900">
-                Get a free sample
-                <span class="font-normal text-gray-500">
-                  (20MB)
-                </span>
-              </a>
+          </div>
+          <div class="mt-10 mx-auto max-w-md lg:m-0 lg:max-w-none lg:col-start-6 lg:col-end-8 lg:row-start-2 lg:row-end-3">
+            <div class="h-full flex flex-col rounded-lg shadow-lg overflow-hidden lg:rounded-none lg:rounded-r-lg">
+              <div class="flex-1 flex flex-col">
+                <div class="bg-white px-6 py-10">
+                  <div>
+                    <h2 class="text-center text-2xl leading-8 font-medium text-gray-900">
+                      2 Bags
+                    </h2>
+                    <div class="mt-4 flex items-center justify-center">
+                      <span class="px-3 flex items-start text-6xl leading-none tracking-tight text-gray-900">
+                        <span class="mt-2 mr-2 text-4xl font-medium">
+                          $
+                        </span>
+                        <span class="font-extrabold">
+                          28
+                        </span>
+                      </span>
+                      <span class="text-xl leading-7 font-medium text-gray-400">
+                        /month
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <div class="flex-1 flex flex-col justify-between border-t-2 border-gray-100 p-6 bg-gray-50 sm:p-10 lg:p-6 xl:p-10">
+                  <ul>
+                    <li class="flex items-start">
+                      <div class="flex-shrink-0">
+                        <svg class="h-6 w-6 text-green-500" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                        </svg>
+                      </div>
+                      <p class="ml-3 text-base leading-6 font-medium text-gray-500">
+                        Pariatur quod similique
+                      </p>
+                    </li>
+                    <li class="mt-4 flex items-start">
+                      <div class="flex-shrink-0">
+                        <svg class="h-6 w-6 text-green-500" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                        </svg>
+                      </div>
+                      <p class="ml-3 text-base leading-6 font-medium text-gray-500">
+                        Sapiente libero doloribus
+                      </p>
+                    </li>
+                    <li class="mt-4 flex items-start">
+                      <div class="flex-shrink-0">
+                        <svg class="h-6 w-6 text-green-500" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                        </svg>
+                      </div>
+                      <p class="ml-3 text-base leading-6 font-medium text-gray-500">
+                        Vel ipsa esse repudiandae
+                      </p>
+                    </li>
+                  </ul>
+                  <div class="mt-8">
+                    <div class="rounded-lg shadow-md">
+                      <a href="#" class="block w-full text-center rounded-lg border border-transparent bg-white px-6 py-3 text-base leading-6 font-medium text-primary-600 hover:text-primary-500 focus:outline-none focus:shadow-outline transition ease-in-out duration-150">
+                        Start your trial
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>

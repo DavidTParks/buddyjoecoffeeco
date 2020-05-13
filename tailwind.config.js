a12c63a5e2831a2b09d8acc7f517bfb7838721e0
David Parks <davidthomasparks@gmail.com>
2020-05-12 23:43:51 -0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/*
+** TailwindCSS Configuration File
+**
+** Docs: https://tailwindcss.com/docs/configuration
+** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+*/
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          100: "#00A5CE",
+          200: "#00A5CE",
+          300: "#00A5CE",
+          400: "#00A5CE",
+          500: "#00A5CE",
+          600: "#00A5CE",
+          700: "#00A5CE",
+          800: "#00A5CE"
+        }
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+};

fa2bd90e9c42fdce1b1829c2611d000370ea7ed5
David Parks <davidthomasparks@gmail.com>
2020-05-13 11:33:44 -0500

    favicon and meta tags

--- components/PricingSection.vue
+++ components/PricingSection.vue
@@ -19,7 +19,7 @@
         <div class="max-w-lg mx-auto rounded-lg shadow-lg overflow-hidden lg:max-w-none lg:flex">
           <div class="bg-white px-6 py-8 lg:flex-shrink-1 lg:p-12">
             <h3 class="text-2xl leading-8 font-extrabold text-gray-900 sm:text-3xl sm:leading-9">
-              Buddy Joe Membership
+              Buddy Joe Coffee Club Membership
             </h3>
             <p class="mt-6 text-base leading-6 text-gray-500">
               Lorem ipsum dolor sit amet consect etur adipisicing elit. Itaque amet indis perferendis blanditiis repellendus etur quidem assumenda.
